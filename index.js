5141b6835989ea44fb3d97eb803e31dceec74547
Braden Hitchcock <developer.hitchcock@gmail.com>
2018-10-29 21:43:51 -0700

    basic zipping to local file system

---
+++ index.js
@@ -0,0 +1,17 @@
+const backup = require('./lib/backup');
+const config = require('./lib/config');
+
+process.env.BR_CONFIG = __dirname;
+
+(async function() {
+    try{
+        await config.load();
+
+        let files = ['config.json', 'lib'];
+
+        backup.run('', false, files);
+    } catch(err){
+        console.log(err);
+    }
+    
+})();
