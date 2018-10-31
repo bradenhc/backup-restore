f3b55d92488278258ec4f56bd1918672d104acf0
Braden Hitchcock <developer.hitchcock@gmail.com>
2018-10-31 09:16:39 -0700

    remove event emitter dependency, create backup command for bin

---
+++ bin/br.js
@@ -0,0 +1,25 @@
+const backup = require('../lib/backup');
+const restore = require('../lib/restore');
+const config = require('../lib/config');
+const program = require('commander');
+
+program
+    .version('1.0.0', '-v, --version')
+    .description('Backup and restore files');
+
+program
+    .command('backup <dirs...>')
+    .description('Backup the provided directories')
+    .option('-n, --name <name>', 'name of the backup', 'br-backup-' + new Date().toDateString().replace(/\s/g, '-'))
+    .option('-e, --encrypt', 'whether or not to encrypt the backup files', false)
+    .action(_backup);
+
+program.parse(process.argv);
+
+function _backup(files, options){
+    try {
+        backup.run(options.name, options.encrypt, files);
+    } catch(err){
+        console.log(err);
+    }
+}
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const backup = require('./lib/backup');
 const config = require('./lib/config');
 
-process.env.BR_CONFIG = __dirname;
-
 (async function() {
     try{
         await config.load();
