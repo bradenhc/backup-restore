0c494800f7b049d3ee15beaa84c1b600fa20a3e7
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-01-13 17:18:05 -0800

    saving most recent changes

--- bin/br.js
+++ bin/br.js
@@ -1,9 +1,8 @@
 const backup = require('../lib/backup');
+const restore = require('../lib/restore');
 const program = require('commander');
 
-program
-    .version('1.0.0', '-v, --version')
-    .description('Backup and restore files');
+program.version('1.0.0', '-v, --version').description('Backup and restore files');
 
 program
     .command('backup <dirs...>')
@@ -12,18 +11,50 @@ program
     .option('-e, --encrypt', 'whether or not to encrypt the backup files', false)
     .action(_backup);
 
+program
+    .command('restore <name> [<destination>]')
+    .description('Restore the files previously backed up under the provided name to the given destination')
+    .action(_restore);
+
+program
+    .command('list')
+    .description('List the available backups under the currently configured destination')
+    .action(_list);
+
 program.parse(process.argv);
 
-async function _backup(dirs, options){
+async function _backup(dirs, options) {
     try {
         await backup.init(options.name, options.encrypt);
         let files = [];
-        for(let dir of dirs){
+        for (let dir of dirs) {
             files = files.concat(await backup.gather(dir));
             console.log(files.length);
         }
         await backup.compress(files);
-    } catch(err){
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function _restore(name, destination) {
+    try {
+        await restore.init(name, destination);
+        await restore.start();
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function _list() {
+    try {
+        let files = await restore.list();
+        console.log('');
+        for(let f of files) {
+            console.log('    ' + f);
+        }
+        console.log('');
+    } catch(err) {
         console.log(err);
     }
 }
